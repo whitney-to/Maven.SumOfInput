8b6690b5509c217e61b80ff3012bdb86ec68a090
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-01 17:16:37 -0500

    finished function of methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -9,18 +11,27 @@ public class Main {
         
         int sum = sumOfNumbers(number);
         
-        System.out.println(sum);
+        System.out.println("sumOfNumbers method: "+sum);
 
+        System.out.println("gaussianSumOfNumbers method: "+gaussianSumOfNumbers(number));
+
+        compareMethodsTime(100000);
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int i = 0;
+        int result = 0;
+        while(i<=n){
+            result+=i;
+            i++;
+        }
+        return result;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -32,14 +43,37 @@ public class Main {
         return num;
     }
     
-    
-    
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        if(n%2==0){
+            return (n/2)*(n+1);
+        } else {
+            double temp = n;
+            return (int) ((temp/2.0)*(n+1));
+        }
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+    static void compareMethodsTime(int number){
+        // timing sumOfNumbers method
+        long start = System.currentTimeMillis();
+            sumOfNumbers(number);
+        long end = System.currentTimeMillis();
+        long dif1 = end - start;
+
+        // timing gaussianSumOfNumbers method
+        start = System.currentTimeMillis();
+            gaussianSumOfNumbers(number);
+        end = System.currentTimeMillis();
+        long dif2 = end - start;
+
+        // check if sumOfNumbers method take less time
+        if(dif1<dif2){
+            System.out.format("For number = %d, %s is faster",number,"sumOfNumbers method");
+        } else { // check otherwise
+            System.out.format("For number = %d, %s is faster",number,"gaussianSumOfNumbers method");
+        }
+    }
 }
